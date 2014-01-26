d0b6402bd09edc280877464226e2707c6658e35f
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-01-26 23:05:35 +0100

    use an array for the article list

--- bin/json2rss.js
+++ bin/json2rss.js
@@ -20,28 +20,26 @@ var feed = new RSS({
 });
 
 var maxItems = 20;
-var itemCount = 0;
 
-function getItemContent(item, itemPath) {
-  var htmlfile = fs.readFileSync(itemPath+'/'+item + argv.itemSuffix, 'utf-8')
+function getItemContent(slug, itemPath) {
+  var htmlfile = fs.readFileSync(itemPath+'/'+slug + argv.itemSuffix, 'utf-8')
   var $ = cheerio.load(htmlfile);
   return $(argv.itemSelector).html();
 };
 
-for(var item in file){
-  if(itemCount >= maxItems) break;
-  itemCount++;
+file.list.forEach(function(item, index){
+  if(index+1 >= maxItems) return;
   feed.item({
-    title: file[item].title,
-    description: argv.itemsPath ? getItemContent(item, argv.itemsPath) :'',
-    date: file[item].date,
-    guid: item,
-    author: file[item].author || argv.author || '',
-    url: file[item].url || argv.siteUrl ?
-      argv.siteUrl +'/'+ (argv.itemPrefix || '') + item + (argv.itemSuffix || '')
+    title: item.title,
+    description: argv.itemsPath ? getItemContent(item.slug, argv.itemsPath) :'',
+    date: item.date,
+    guid: item.slug,
+    author: item.author || argv.author || '',
+    url: item.url || argv.siteUrl ?
+      argv.siteUrl +'/'+ (argv.itemPrefix || '') + item.slug + (argv.itemSuffix || '')
       : ''
   });
-}
+});
 
 fs.writeFileSync(outputFile, feed.xml(true));
 console.log('feed generated at '+outputFile);
